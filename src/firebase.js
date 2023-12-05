2784e84ddc7b1d2515b71aafa77d9ab979283e33
Selim Kaya <136734877+selimthebrogrammer@users.noreply.github.com>
2023-12-05 10:48:04 +0300

    firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBv6zEONDVJSxOlkxmyUVto6eWksYVXwSU",
-    authDomain: "eshop-c2e0a.firebaseapp.com",
-    projectId: "eshop-c2e0a",
-    storageBucket: "eshop-c2e0a.appspot.com",
-    messagingSenderId: "941031253944",
-    appId: "1:941031253944:web:0bc5a43bc9fe9af86e514d",
-    measurementId: "G-5BDLQRB305"
+    apiKey: "YOUR_API",
+    authDomain: "YOUR_AUTHDOMAIN",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "YOUR_APP_ID",
+    measurementId: "YOUR_MEASURE_ID"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
